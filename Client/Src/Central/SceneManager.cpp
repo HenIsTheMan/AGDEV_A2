9d378ac685284e7219c9a7b5a256fedc36c0469b
Henry <193541T@mymail.nyp.edu.sg>
2021-02-02 13:34:19 +0800

    Nah

--- Client/Src/Central/SceneManager.cpp
+++ Client/Src/Central/SceneManager.cpp
@@ -1,6 +1,52 @@
 #include "SceneManager.h"
 
 SceneManager::SceneManager():
-	im_GameScene(new GameScene())
+	im_GameScene(new GameScene()),
+	im_GameInCtor(nullptr),
+	im_GameInDtor(nullptr),
+	im_GameInit(nullptr),
+	im_GameFixedUpdate(nullptr),
+	im_GameUpdate(nullptr),
+	im_GameLateUpdate(nullptr),
+	im_GamePreRender(nullptr),
+	im_GameRender(nullptr),
+	im_GamePostRender(nullptr)
 {
+}
+
+void SceneManager::Init(){
+}
+
+struct CBase
+{ 
+};
+
+struct CDerived0 : CBase
+{
+    void MyFunction()
+    {
+        std::cout << 0 << std::endl;
+    }
+};
+
+struct CDerived1 : CBase
+{
+    void MyFunction()
+    {
+        std::cout << 1 << std::endl;
+    }
+};
+
+
+typedef void (CBase::*FunctionPointer)();
+
+
+int main()
+{
+    CDerived0* derived0 = new CDerived0();
+
+    FunctionPointer pointer = static_cast<FunctionPointer>(&CDerived0::MyFunction);
+    (derived0->*pointer)();
+
+    delete derived0;
 }
--- Client/Src/Central/SceneManager.h
+++ Client/Src/Central/SceneManager.h
@@ -8,8 +8,20 @@ class SceneManager final: public Singleton<SceneManager>{
 	friend Singleton<SceneManager>;
 public:
 	~SceneManager() = default;
+
+	void Init();
 private:
 	GameScene* im_GameScene;
+
+	void (*im_GameInCtor)();
+	void (*im_GameInDtor)();
+	void (*im_GameInit)();
+	void (*im_GameFixedUpdate)();
+	void (*im_GameUpdate)();
+	void (*im_GameLateUpdate)();
+	void (*im_GamePreRender)();
+	void (*im_GameRender)();
+	void (*im_GamePostRender)();
 
 	SceneManager();
 };
